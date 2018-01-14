a88fa21ef31658b01f4836b852116deaa6ec5ccc
Andrew Pashinnik <tobirawork@gmail.com>
2018-01-14 16:01:55 +0200

    feat(client): add hasBody method, set defaults values rename status method to checkSatatus

--- src/client.js
+++ src/client.js
@@ -1,30 +1,56 @@
-export default class Client {
-    constructor (options) {
-        this.defaults = options
+class Client {
+    constructor (settings) {
+        this.defaults = settings
+
+        if (!this.defaults.mode) this.defaults.mode = 'cors'
+        if (!this.defaults.credentials) this.defaults.credentials = 'omit'
+        if (!this.defaults.redirect) this.defaults.redirect = 'follow'
+        if (!this.defaults.cache) this.defaults.cache = 'default'
+
+        // console.log(this.defaults)
     }
 
     request (options) {
-        let { url, method, body, params } = options
-        const init = {}
+        let {
+            credentials,
+            url,
+            method,
+            body,
+            params,
+            headers,
+            mode,
+            redirect,
+            cache
+        } = options
+
+        const REQUEST_OPTIONS = {}
+        const URL = this.normalizeURL(url, params)
         
-        url = this.defaults.baseURL + url
-        method = method.toUpperCase()
-        init.method = method
+        REQUEST_OPTIONS.credentials = credentials || this.defaults.credentials
+        REQUEST_OPTIONS.redirect = redirect || this.defaults.redirect
+        REQUEST_OPTIONS.method = method
+        REQUEST_OPTIONS.cache = redirect || this.defaults.cache
+        REQUEST_OPTIONS.mode = mode || this.defaults.mode
+        REQUEST_OPTIONS.headers = new Headers(headers || this.defaults.headers)
+        
+        if (this.hasBody(method)) REQUEST_OPTIONS.body = body
 
-        if (method === 'GET' || method === 'HEAD') {
-            if (params) url = `${url}?${JSON.stringify(params)}`
-        } else {
-            init.body = body
-        }
+        console.log(REQUEST_OPTIONS)
 
-        const request = this.intercept('req', new Request(url, init))
+        // TODO: Improve Request interceptor
+        const request = this.intercept('req', new Request(URL, REQUEST_OPTIONS))
 
         return fetch(request)
-            .then(this.status)
-            .then(res => this.intercept('res', res))
+            .then(this.checkStatus)
+            .then(response => this.intercept('res', response))
     }
 
-    status (res) {
+    hasBody (method) {
+        method = method.toUpperCase()
+        return (method === 'POST' || method === 'PUT' || method === 'PATCH')
+    }
+
+    checkStatus (res) {
         if (res.status >= 200 && res.status < 300) {
             return Promise.resolve(res)
         } else {
@@ -39,4 +65,13 @@ export default class Client {
             return obj
         }
     }
+
+    normalizeURL (relativeURL, params) {
+        let url = this.defaults.baseURL + relativeURL
+        if (params) url = url + '?' + JSON.stringify(params)
+
+        return  url
+    }
 }
+
+export default Client
