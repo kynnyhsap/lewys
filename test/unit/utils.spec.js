67829734916d7fb51572d25dd685c5505bb1f7f4
Andrew Pashinnik <tobirawork@gmail.com>
2018-01-21 20:06:11 +0200

    test(unit): change all 'test' to 'it'

--- test/unit/utils.spec.js
+++ test/unit/utils.spec.js
@@ -5,11 +5,11 @@ describe('Utils', () => {
         const data = { lol: 'kek' }
         const interceptor = (obj) => obj.lol
     
-        test('should handle passed object', () => {
+        it('should handle passed object', () => {
             expect(utils.intercept(data, interceptor)).toBe('kek')
         })
     
-        test('should return headled by default object', () => {
+        it('should return headled by default object', () => {
             expect(utils.intercept(data)).toBe(data)
         })
     })
@@ -22,14 +22,14 @@ describe('Utils', () => {
             }, 3000)
         })
 
-        test('should reject on timeout', () => {
+        it('should reject on timeout', () => {
             expect.assertions(1)
             return expect(utils.startTimeout(promise, 1000, errMessage))
                 .rejects
                 .toMatch(errMessage)
         })
 
-        test('should resolve if time left', () => {
+        it('should resolve if time left', () => {
             expect.assertions(1)
             return expect(utils.startTimeout(promise, 5000, errMessage))
                 .resolves
@@ -38,7 +38,7 @@ describe('Utils', () => {
     })
 
     describe('handleStatus', () => {
-        test('should resolve if status between 200 and 300', () => {
+        it('should resolve if status between 200 and 300', () => {
             const niceRes = { status: 200, statusText: 'Ok' }
 
             expect.assertions(1)
@@ -47,7 +47,7 @@ describe('Utils', () => {
                 .toEqual(niceRes)
         })
 
-        test('sould reject if status is ivalid', () => {
+        it('sould reject if status is ivalid', () => {
             const badRes = { status: 404, statusText: 'Not found' }
             const err = new Error(badRes.statusText)
 
@@ -62,30 +62,30 @@ describe('Utils', () => {
         const params = { lol: 'kek' }
         const serializer = (params) => 'lol=kek'
     
-        test('should handle params by callback', () => {
+        it('should handle params by callback', () => {
             expect(utils.paramsSerializer(params, serializer)).toBe('lol=kek')
         })
     
-        test('should return stringified params', () => {
+        it('should return stringified params', () => {
             expect(utils.paramsSerializer(params)).toBe(JSON.stringify(params))
         })
     })
 
     describe('cunstructURL', () => {
-        test('should concat baseURL, relativeURL and params(if exist) to single string', () => {
-            const baseURL = 'https://test.some'
+        it('should concat baseURL, relativeURL and params(if exist) to single string', () => {
+            const baseURL = 'https://it.some'
             const relativeURL = '/api/post/1'
             const params = 'some=10'
         
             const url = utils.constructURL(baseURL, relativeURL)
             const urlWithParams = utils.constructURL(baseURL, relativeURL, params)
         
-            expect(url).toBe('https://test.some/api/post/1')
-            expect(urlWithParams).toBe('https://test.some/api/post/1?some=10')
+            expect(url).toBe('https://it.some/api/post/1')
+            expect(urlWithParams).toBe('https://it.some/api/post/1?some=10')
         })
 
-        test('should ignore baseURL if relativeURL is fully', () => {
-            const baseURL = 'https://test.some'
+        it('should ignore baseURL if relativeURL is fully', () => {
+            const baseURL = 'https://it.some'
             const relativeURL = 'http://some.other/api'
         
             expect(utils.constructURL(baseURL, relativeURL)).toBe(relativeURL)
@@ -93,7 +93,7 @@ describe('Utils', () => {
     })
 
     describe('hasBody', () => {
-        test('should check if request method could have a body', () => {
+        it('should check if request method could have a body', () => {
             const POST = 'POST'
             const PUT = 'PUT'
             const PATCH = 'PATCH'
@@ -107,7 +107,7 @@ describe('Utils', () => {
     })
 
     describe('getWritbleOptions', () => {
-        test('sould return writble object', () => {
+        it('sould return writble object', () => {
             const object = {
                 kek: 'lol',
                 got () {}
