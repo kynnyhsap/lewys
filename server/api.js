3c0fc1c00907f582975c23d6a6d3a21a204910dd
Andrew Pashinnik <tobirawork@gmail.com>
2018-01-21 22:25:37 +0200

    chore(server): some styling for data manegment

---
+++ server/api.js
@@ -0,0 +1,29 @@
+import lewys from '../src'
+
+const api = lewys.init({
+    baseURL: 'https://jsonplaceholder.typicode.com',
+
+    timeout: 10000,
+
+    // handleStatus(res) {
+    //     cosole.log(res)
+    //     return Promise.resolve(res)
+    // },
+
+    // serializer (params) {
+    //     return JSON.stringify(params)
+    // },
+
+    beforeResponse (res, writble) {
+        console.log(res)
+        return res.json().then(data => data)
+    },
+
+    beforeRequest (req, writble) {
+        console.log(req)
+        return req
+    },
+})
+
+export default api
+
