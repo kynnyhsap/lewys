30b362e677f1f1efd2ff1279acd1f23365424969
Andrew Pashinnik <tobirawork@gmail.com>
2018-02-04 13:59:17 +0200

    refactor(client): make unused by lib options custom, move headers, body and method options to OPTION definition, use itercept from params istead of removed paramsSerializer

--- src/client.js
+++ src/client.js
@@ -5,35 +5,30 @@ class Client {
         this.defaults = settings
         this.controller = undefined
 
-        if (!this.defaults.credentials) this.defaults.credentials = 'omit'
-        if (!this.defaults.redirect) this.defaults.redirect = 'follow'
         if (!this.defaults.timeout) this.defaults.timeout = 30000
-        if (!this.defaults.cache) this.defaults.cache = 'default'
-        if (!this.defaults.mode) this.defaults.mode = 'cors'
     }
 
-    request ({ url, method, body, params, headers, credentials, mode, redirect, cache }) {
-        const OPTIONS = {}
+    request ({ url, method, body, params, headers, ...customOptions }) {
+        const OPTIONS = {
+            headers: new Headers(headers || this.defaults.headers),
+            body: utils.hasBody(method) ? body : undefined,
+            method: method || 'GET',
+            ...customOptions,
+        }
 
-        if (utils.hasBody(method)) OPTIONS.body = body
+        const URL = utils.makeUrl({
+            relative: url,
+            base: this.defaults.baseURL,
+            params: utils.intercept(
+                params,
+                this.defaults.serializer || JSON.stringify
+            )
+        })
 
         if ('AbortController' in window) {
             this.controller = new AbortController()
             OPTIONS.signal = this.controller.signal
         }
-
-        OPTIONS.credentials = credentials || this.defaults.credentials
-        OPTIONS.redirect = redirect || this.defaults.redirect
-        OPTIONS.headers = new Headers(headers || this.defaults.headers)
-        OPTIONS.cache = cache || this.defaults.cache
-        OPTIONS.mode = mode || this.defaults.mode
-        OPTIONS.method = method || 'GET'
-
-        const URL = utils.makeUrl({
-            base: this.defaults.baseURL,
-            relative: url,
-            params: utils.paramsSerializer(params, this.defaults.serializer)
-        })
 
         const request = utils.intercept(
             new Request(URL, OPTIONS),
