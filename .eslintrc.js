4bbdc5c4a4df41c0494d2abda9d0e8fae4ee1fcd
Andrew Pashinnik <tobirawork@gmail.com>
2018-02-06 22:33:08 +0200

    chore: add prettier config, correct eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,16 +7,11 @@ module.exports = {
         jest: true,
     },
 
-    'rules': {
-        // allow paren-less arrow functions
-        'arrow-parens': 0,
-        // allow async-await
-        'generator-star-spacing': 0,
+    rules: {
         // 4 spaces
-        'indent': ['error', 4],
-        // allow comma
-        "comma-dangle": 0,
-        // allow 1-2 epmty lines
-        "no-multiple-empty-lines": ["error", { "max": 2, "maxBOF": 1}]
-    }
+        indent: ['error', 4],
+
+        'comma-dangle': 0,
+        'space-before-function-paren': ['error', 'never'],
+    },
 }
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: false,
+    tabWidth: 4,
+    useTabs: false,
+    printWidth: 80,
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+}
